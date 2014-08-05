1390a7eaefc2e51fc4d1140091a167514c42320c
tobes <toby.junk@gmail.com>
2014-08-05 12:44:45 +0100

    Update setup name to sort import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = '0.1'
 
 setup(
-    name='ckanext-mopac',
+    name='ckanext-client-mopac',
     version=version,
     description="Mopac client extension",
     long_description='''
